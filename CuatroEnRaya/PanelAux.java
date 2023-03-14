13a2772dbc7c78716b7c92c468fabf1c9c1e4835
Estebanq3 <estebanq52@gmail.com>
2023-03-14 11:30:31 -0600

    Cuatro en Raya game project
    Game project created using Java and BlueJ

---
+++ CuatroEnRaya/PanelAux.java
@@ -0,0 +1,13 @@
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+
+public class PanelAux extends JPanel
+{
+	public PanelAux(int tamaño)
+	{
+		setPreferredSize(new Dimension(0,tamaño));
+	}
+
+}
